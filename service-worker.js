402ffaeb6c61507be20c92a26a147e8f5a6165c5
francis <franzdiaz460@gmail.com>
2023-07-05 16:08:09 +0400

    ref: able to store updated tracked sites to local storage

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,36 @@
+chrome.runtime.onInstalled.addListener(({ reason }) => {
+	reason === "install" && chrome.storage.local.set({ trackedSites: [] });
+});
+
 chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
 	if (message.track) {
 		const [{ url, title }] = await chrome.tabs.query({
 			active: true,
 			lastFocusedWindow: true,
 		});
+		const currentActiveTab = {
+			url,
+			title,
+			isTracked: true,
+			timesVisited: 1,
+			time: {
+				initialTrackedTime: new Date(),
+				currentTrackedTime: new Date(),
+			},
+		};
 		console.log({ url, title });
+		const storage = await chrome.storage.local.get(["trackedSites"]);
+		console.log(storage);
+		await chrome.storage.local.set({
+			trackedSites: [currentActiveTab, ...storage.trackedSites],
+		});
 	}
+});
+
+chrome.storage.onChanged.addListener(async () => {
+	const getStorage = await chrome.storage.local
+		.get(["trackedSites"])
+		.then((result) => {
+			console.log(result.trackedSites);
+		});
 });
