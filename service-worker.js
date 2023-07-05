bfc62ed413e12de499625922c8f4287acc6e2c29
francis <franzdiaz460@gmail.com>
2023-07-05 15:39:14 +0400

    ref: desctructuring relevant information to store inf local storage

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,9 @@
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-	console.log(message.greeting);
-	sendResponse({ response: "lol" });
+chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
+	if (message.track) {
+		const [{ url, title }] = await chrome.tabs.query({
+			active: true,
+			lastFocusedWindow: true,
+		});
+		console.log({ url, title });
+	}
 });
