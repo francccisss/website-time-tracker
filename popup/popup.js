bc683b743181f82c80a2fe4962fc48ee97366fae
francis <franzdiaz460@gmail.com>
2023-07-10 20:15:19 +0400

    ref: cleaned up css button

--- popup/popup.js
+++ popup/popup.js
@@ -1,6 +1,28 @@
-const clickMe = document.getElementById("click-me");
+const slideBtn = document.getElementById("slide-btn");
 
-clickMe.addEventListener("click", async () => {
-	console.log("onClick");
-	const { response } = await chrome.runtime.sendMessage({ track: true });
+function animateSlideButton(e) {
+  e.preventDefault();
+  let target = e.target;
+  // const btnAnimationDuration = target.style.before;
+  // console.log(btnAnimationDuration);
+  let animationDuration =
+    window.getComputedStyle(target).animationDuration.slice(0, -1) * 1000;
+  console.log(animationDuration);
+  // if (target.classList.contains("isNotTracked")) {
+  //   return setTimeout(() => {
+  //     target.classList.replace("isNotTracked", "isTracked");
+  //   }, animationDuration);
+  // }
+  // setTimeout(() => {
+  //   target.classList.replace("isTracked", "isNotTracked");
+  // }, animationDuration);
+  if (target.classList.contains("isNotTracked")) {
+    return target.classList.replace("isNotTracked", "isTracked");
+  }
+  target.classList.replace("isTracked", "isNotTracked");
+}
+slideBtn.addEventListener("click", async (e) => {
+  console.log("onClick");
+  animateSlideButton(e);
+  // const { response } = await chrome.runtime.sendMessage({ track: true });
 });
