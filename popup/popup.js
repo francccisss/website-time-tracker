10f63bccc5e6f17431987aada4b8f1a713e32245
francis <franzdiaz460@gmail.com>
2023-07-10 20:28:09 +0400

    ref: temporary fix for animation, need to make it reusable instead of repeating keyframe declaration

--- popup/popup.js
+++ popup/popup.js
@@ -3,19 +3,8 @@ const slideBtn = document.getElementById("slide-btn");
 function animateSlideButton(e) {
   e.preventDefault();
   let target = e.target;
-  // const btnAnimationDuration = target.style.before;
-  // console.log(btnAnimationDuration);
   let animationDuration =
     window.getComputedStyle(target).animationDuration.slice(0, -1) * 1000;
-  console.log(animationDuration);
-  // if (target.classList.contains("isNotTracked")) {
-  //   return setTimeout(() => {
-  //     target.classList.replace("isNotTracked", "isTracked");
-  //   }, animationDuration);
-  // }
-  // setTimeout(() => {
-  //   target.classList.replace("isTracked", "isNotTracked");
-  // }, animationDuration);
   if (target.classList.contains("isNotTracked")) {
     return target.classList.replace("isNotTracked", "isTracked");
   }
