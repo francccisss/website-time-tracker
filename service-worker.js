bae1663145e7cf8cfe4b927aa994d9c631c7331c
francis <franzdiaz460@gmail.com>
2023-07-11 21:39:46 +0400

    fix: need to desctructure message sender and access sender url within the object

--- service-worker.js
+++ service-worker.js
@@ -1,130 +1,133 @@
 import { getCurrentActiveTab } from "./utils/service-worker.utils.js";
 
 chrome.runtime.onInstalled.addListener(({ reason }) => {
-  reason === "install" && chrome.storage.local.set({ trackedSites: [] });
+	reason === "install" && chrome.storage.local.set({ trackedSites: [] });
 });
 
 chrome.runtime.onMessage.addListener(async ({ track }) => {
-  if (track) {
-    const [{ url, title, favIconUrl }] = await chrome.tabs.query({
-      active: true,
-      lastFocusedWindow: true,
-    });
-    const currentActiveTab = await getCurrentActiveTab(url);
-    if (currentActiveTab !== undefined) {
-      const updateTrackedSites = trackedSites.map((site) => {
-        if (site.url.includes(currentActiveTab.url)) {
-          const updateCurrentTab = {
-            ...site,
-            isTracked: site.isTracked ? false : true,
-            timesVisited: site.timesVisited + 1,
-            time: {
-              ...site.time,
-              currentTrackedTime: currentTime,
-            },
-          };
-          return updateCurrentTab;
-        }
-        return site;
-      });
-      await chrome.storage.local.set({
-        trackedSites: updateTrackedSites,
-      });
-    } else if (currentActiveTab === undefined) {
-      const createCurrentTabData = {
-        url: new URL(url).hostname,
-        title,
-        favIconUrl,
-        isTracked: true,
-        timesVisited: 0,
-        time: {
-          currentTrackedTime: currentTime,
-          totalTrackedTime: currentTime,
-        },
-      };
-      const { trackedSites } = await chrome.storage.local.get(["trackedSites"]);
-      await chrome.storage.local.set({
-        trackedSites: [createCurrentTabData, ...trackedSites],
-      });
-    }
-  }
+	if (track) {
+		const [{ url, title, favIconUrl }] = await chrome.tabs.query({
+			active: true,
+			lastFocusedWindow: true,
+		});
+		const currentActiveTab = await getCurrentActiveTab(url);
+		if (currentActiveTab !== undefined) {
+			const updateTrackedSites = trackedSites.map((site) => {
+				if (site.url.includes(currentActiveTab.url)) {
+					const updateCurrentTab = {
+						...site,
+						isTracked: site.isTracked ? false : true,
+						timesVisited: site.timesVisited + 1,
+						time: {
+							...site.time,
+							currentTrackedTime: currentTime,
+						},
+					};
+					return updateCurrentTab;
+				}
+				return site;
+			});
+			await chrome.storage.local.set({
+				trackedSites: updateTrackedSites,
+			});
+		} else if (currentActiveTab === undefined) {
+			const createCurrentTabData = {
+				url: new URL(url).hostname,
+				title,
+				favIconUrl,
+				isTracked: true,
+				timesVisited: 0,
+				time: {
+					currentTrackedTime: currentTime,
+					totalTrackedTime: currentTime,
+				},
+			};
+			const { trackedSites } = await chrome.storage.local.get([
+				"trackedSites",
+			]);
+			await chrome.storage.local.set({
+				trackedSites: [createCurrentTabData, ...trackedSites],
+			});
+		}
+	}
 });
 
 chrome.storage.onChanged.addListener(async () => {
-  await chrome.storage.local.get(["trackedSites"]).then((result) => {
-    console.log(result.trackedSites);
-  });
+	await chrome.storage.local.get(["trackedSites"]).then((result) => {
+		console.log(result.trackedSites);
+	});
 });
 
 // need to listen to different events and inject script to tabs that is currently active
 // injects on every tabs that were active and establishes a connection
 chrome.history.onVisited.addListener(async () => {
-  const [{ id }] = await chrome.tabs.query({
-    active: true,
-    lastFocusedWindow: true,
-  });
-  try {
-    await chrome.scripting.executeScript({
-      target: { tabId: id },
-      files: ["/content-script/connection.js"],
-    });
-    console.log("successfully injected");
-  } catch (err) {
-    console.log("unable to inject script");
-  }
+	const [{ id }] = await chrome.tabs.query({
+		active: true,
+		lastFocusedWindow: true,
+	});
+	try {
+		await chrome.scripting.executeScript({
+			target: { tabId: id },
+			files: ["/content-script/connection.js"],
+		});
+		console.log("successfully injected");
+	} catch (err) {
+		console.log("unable to inject script");
+	}
 });
 
 // problem if a user is not active in a connected tab, the time will still be counted from the first visit and the difference
 // from when it was inactive and only calculates the total time after it disconnects
 // > need to only set a time if the tab is active and calculate total time on disconnect and inactive tab event
 chrome.runtime.onConnect.addListener(async (port) => {
-  console.log("connected");
-  let trackedTabUrl;
-  if (port.name === "connect") {
-    port.onMessage.addListener(({ sender }) => {
-      trackedTabUrl = sender.url;
-    });
-    port.onDisconnect.addListener(async () => {
-      console.log("disconnected");
-      const currentActiveTab = await getCurrentActiveTab(trackedTabUrl);
-      if (currentActiveTab !== undefined) {
-        console.log(currentActiveTab);
-      } else {
-        console.log("is not in database");
-      }
-    });
-  }
+	console.log("connected");
+	let trackedTabUrl;
+	if (port.name === "connect") {
+		port.onMessage.addListener((msg, { sender }) => {
+			console.log(msg.greetings);
+			trackedTabUrl = sender.url;
+		});
+		port.onDisconnect.addListener(async () => {
+			console.log("disconnected");
+			const currentActiveTab = await getCurrentActiveTab(trackedTabUrl);
+			if (currentActiveTab !== undefined) {
+				console.log(currentActiveTab);
+			} else {
+				console.log("is not in database");
+			}
+		});
+	}
 });
 
 chrome.history.onVisited.addListener(async ({ url }) => {
-  const { trackedSites } = await chrome.storage.local.get(["trackedSites"]);
-  const currentActiveTab = await getCurrentActiveTab(url);
-  if (currentActiveTab !== undefined) {
-    const currentTime = Date.now();
-    const updateTrackedSites = trackedSites.map((site) => {
-      if (
-        site.url.includes(currentActiveTab.url) &&
-        currentActiveTab.isTracked
-      ) {
-        const updateCurrentTab = {
-          ...site,
-          timesVisited: site.timesVisited + 1,
-          time: {
-            ...site.time,
-            currentTrackedTime: currentTime,
-          },
-        };
-        return updateCurrentTab;
-      } else {
-        return site;
-      }
-    });
-    await chrome.storage.local.set({
-      trackedSites: updateTrackedSites,
-    });
-  }
+	const { trackedSites } = await chrome.storage.local.get(["trackedSites"]);
+	const currentActiveTab = await getCurrentActiveTab(url);
+	if (currentActiveTab !== undefined) {
+		const currentTime = Date.now();
+		const updateTrackedSites = trackedSites.map((site) => {
+			if (
+				site.url.includes(currentActiveTab.url) &&
+				currentActiveTab.isTracked
+			) {
+				const updateCurrentTab = {
+					...site,
+					timesVisited: site.timesVisited + 1,
+					time: {
+						...site.time,
+						currentTrackedTime: currentTime,
+					},
+				};
+				return updateCurrentTab;
+			} else {
+				return site;
+			}
+		});
+		await chrome.storage.local.set({
+			trackedSites: updateTrackedSites,
+		});
+	}
 });
 
 chrome.tabs.onRemoved.addListener(async () => {
-  console.log("tab removed");
+	console.log("tab removed");
 });
